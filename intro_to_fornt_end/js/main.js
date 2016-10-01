0c0ee8d92c9c042f4b2aaab322b3659a428276a0
Rabra Hierpa <rabira.hierpa@gmail.com>
2016-10-01 17:43:54 +0300

    Testing Conditionals in the js file
    Added a conditional statment in the main.js file
    but this time I am trying to stage,commit and push using GitKraken

--- intro_to_fornt_end/js/main.js
+++ intro_to_fornt_end/js/main.js
@@ -1,2 +1,9 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+// Conditionals in java
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
